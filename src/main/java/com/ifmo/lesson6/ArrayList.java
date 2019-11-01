5284fa9cdb7dca40008e420d56fe7b4cc7c9a195
kozlo <adm589@mail.ru>
2019-11-02 01:24:05 +0300

    Lesson6 mod

--- src/main/java/com/ifmo/lesson6/ArrayList.java
+++ src/main/java/com/ifmo/lesson6/ArrayList.java
@@ -66,14 +66,14 @@ public class ArrayList implements List {
     @Override
     public Object get(int i) {
         // TODO implement.
-        return (i >= values.length) ? null : values[i];
+        return (i > values.length) ? null : values[i];
     }
 
     /** {@inheritDoc} */
     @Override
     public Object remove(int i) {
         // TODO implement.
-        if (i >= values.length)
+        if (i > values.length)
             return null;
 
         Object result = values[i];
@@ -82,7 +82,7 @@ public class ArrayList implements List {
                 values[j - 1] = values[j];
         }
 
-        values[values.length] = null;
+        values[values.length - 1] = null;
 
         return result;
     }
